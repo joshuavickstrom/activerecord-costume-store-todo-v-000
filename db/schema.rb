b3ab4458e02dc93e0ffbef43a3a9ed5b09d1819c
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-05 18:50:15 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,6 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "costume_stores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "costume_inventory"
-    t.integer "employees"
-    t.boolean "still_in_business"
-    t.string "opening_time"
-    t.string "closing_time"
-  end
-
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
