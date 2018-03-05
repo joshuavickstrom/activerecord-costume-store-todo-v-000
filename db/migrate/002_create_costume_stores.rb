73aa34c33a9743ce07f067627ebb624d120317d2
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-05 18:50:05 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     add_column :costume_stores, :name, :string
     add_column :costume_stores, :location, :string
     add_column :costume_stores, :costume_inventory, :integer
-    add_column :costume_stores, :employees, :integer
+    add_column :costume_stores, :employee_count, :integer
     add_column :costume_stores, :still_in_business, :boolean
     add_column :costume_stores, :opening_time, :string
     add_column :costume_stores, :closing_time, :string
